d542522e4d30d61fa0b97b351297b219911f8d82
Maria Colta <mcolta@Ellations-iMac-3.local>
2019-08-20 16:41:55 +0300

    Collection and Home

---
+++ src/Collection/Collection.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Colection.css";
+
+function Colection() {
+  return <div className="colection">bcksbcjsls</div>;
+}
+
+export default Colection;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Collection from "../Collection/Collection";
 
 function Home() {
   return <Link to="about">Go To About Page</Link>;
