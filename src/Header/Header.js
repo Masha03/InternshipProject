beb22a6fc0f330fc235e6693bdc4dc743264f62c
Masha <51504503+Masha03@users.noreply.github.com>
2019-08-21 12:33:44 +0300

    Implemented header (#37)
    * header
    
    * Add packa-lock to gitignore
    
    * Deleted comments in Header.css and Home.js

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Header.css";
+import { API_KEY } from "../constant";
 
-function Header() {
-  return <header className="header"></header>;
+function Header(props) {
+  console.log("Header: ", props);
+
+  return (
+    <header className="header">
+      <div className="header-logo">
+        {props.logoText}
+        <img className="logo-image" src={props.image} alt="" />
+      </div>
+      <ul className="header-menu">
+        {props.menu.map(item => (
+          <li className="list-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
---
+++ src/constant.js
@@ -0,0 +1,2 @@
+const API_KEY = "ccfca547c9a28ee57d717d1e072aa89b";
+export { API_KEY };
