f0f8b2ecd20cff317a89fd1c1438bb9a228f4f59
Maria Colta <mcolta@Ellations-iMac-3.local>
2019-08-22 15:46:15 +0300

    Work with API

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,83 +3,23 @@ import "./Card.css";
 import { API_KEY } from "../constant";
 import { functionTypeAnnotation } from "@babel/types";
 
-function Card() {
+function Card({ title, genres, description, src }) {
   return (
-    <section className="collection">
-      <div className="card-item">
-        <img src="https://at-cdn-s01.audiotool.com/2014/09/19/albums/4FlEy3hz/cover256x256-ee92c401539b45f68dc183336846cb9c.jpg" />
-        <div className="movie-details">
-          <h4 className="movie-title">Title</h4>
-          <ul className="genre">
-            <li className="genre-name">Action</li>
-            <li className="genre-name">Crime</li>
-            <li className="genre-name">Sci-fi</li>
-          </ul>
-          <p className="movie-description">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries
-          </p>
-          <button className="button">READ MORE</button>
-        </div>
+    <div className="card-item">
+      <img src={src} className="image" />
+      <div className="movie-details">
+        <h4 className="movie-title">{title}</h4>
+        <ul className="genre">
+          {genres.map((genre, id) => (
+            <li key={id} className="genre-name">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie-description">{description}</p>
+        <button className="button">READ MORE</button>
       </div>
-      <div className="card-item">
-        <img src="https://ecsmedia.pl/c/the-dark-side-of-the-moon-w-iext35454968.jpg" />
-        <div className="movie-details">
-          <h4 className="movie-title">Title</h4>
-          <ul className="genre">
-            <li className="genre-name">Action</li>
-            <li className="genre-name">Crime</li>
-            <li className="genre-name">Sci-fi</li>
-          </ul>
-          <p className="movie-description">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of typ/.
-          </p>
-          <button className="button">READ MORE</button>
-        </div>
-      </div>
-      <div className="card-item">
-        <img src="https://cdn131.picsart.com/302196089095201.jpg?c256x256" />
-        <div className="movie-details">
-          <h4 className="movie-title">Title</h4>
-          <ul className="genre">
-            <li className="genre-name">Action</li>
-            <li className="genre-name">Crime</li>
-            <li className="genre-name">Sci-fi</li>
-          </ul>
-          <p className="movie-description">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries
-          </p>
-          <button className="button">READ MORE</button>
-        </div>
-      </div>
-      <div className="card-item">
-        <img src="http://cdn.shopify.com/s/files/1/2076/4993/collections/Minimal_Luxe_large_5abf8ea8-dcb3-44da-b029-6cd0058d7f9c_1200x1200.png?v=1527680539" />
-        <div className="movie-details">
-          <h4 className="movie-title">Title</h4>
-          <ul className="genre">
-            <li className="genre-name">Action</li>
-            <li className="genre-name">Crime</li>
-            <li className="genre-name">Sci-fi</li>
-          </ul>
-          <p className="movie-description">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a
-          </p>
-          <button className="button">READ MORE</button>
-        </div>
-      </div>
-    </section>
+    </div>
   );
 }
 
--- src/Collection/Collection.js
+++ src/Collection/Collection.js
@@ -1,9 +1,45 @@
 import React from "react";
 import "./Collection.css";
 import Card from "../Card/Card";
+import { API_KEY } from "../constant";
 
-function Collection() {
-  return <Card></Card>;
+class Collection extends React.Component {
+  state = {
+    status: "Succes",
+    movies: [],
+  };
+
+  componentDidMount() {
+    console.log("Collection did mount!");
+    console.log("Collection state: ", this.state);
+    this.setState({ status: "error" });
+
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          movies: response.results.map(movie => {
+            return {
+              title: movie.original_title,
+              description: movie.overview,
+              src: "https://image.tmdb.org/t/p/w500" + movie.poster_path,
+              genres: movie.genre_ids,
+            };
+          }),
+        });
+      });
+  }
+
+  render() {
+    console.log(this.state.movies);
+    return (
+      <div className="collection">
+        {this.state.movies.map(movie => {
+          return <Card title="" genres={[]} description="" src=""></Card>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default Collection;
